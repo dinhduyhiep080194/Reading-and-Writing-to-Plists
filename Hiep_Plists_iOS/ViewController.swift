69fa8f9dc1b9fa8ab2721aea0ab5d31e1f00074d
Dinh Hiep <hiepmanager@Dinhs-Mac.local>
2017-05-12 01:38:19 +0800

    main

--- Hiep_Plists_iOS/ViewController.swift
+++ Hiep_Plists_iOS/ViewController.swift
@@ -9,17 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let myItemKey = "myItem"
+    var myItemValue: String?
+    
+    @IBOutlet weak var txt_value: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadData()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func loadData() {
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true) as NSArray
+        
+        let documentDirectory = paths[0] as! String
+        let path = documentDirectory.appending("mydata.plist")
+        
+        let fileManager = FileManager.default
+        if(!fileManager.fileExists(atPath: path)) {
+            if let bundlePath = Bundle.main.path(forResource: "mydata", ofType: "plist") {
+                let result = NSMutableDictionary(contentsOfFile: bundlePath)
+                print("Bundle File mydata.plist is: -> \(String(describing: result?.description))")
+                do {
+                    try fileManager.copyItem(atPath: bundlePath, toPath: path)
+                } catch {
+                    print("Copy failure")
+                }
+            } else {
+                print("file myData.plist not found")
+            }
+        } else {
+            print("File mydata.plist already at path")
+        }
+        
+        let resultDictionary = NSMutableDictionary(contentsOfFile: path)
+        print("Load mydata.plist is -> \(String(describing: resultDictionary?.description))")
+        
+        let myDict = NSDictionary(contentsOfFile: path)
+        if let dict = myDict {
+            myItemValue = dict.object(forKey: myItemKey) as? String
+            txt_value.text = myItemValue
+        } else {
+            print("Load Failure")
+        }
+    }
 }
+
 
