7d5f39a1db848c0f1519beef6a0358285a28b6ef
Dinh Hiep <hiepmanager@Dinhs-Mac.local>
2017-05-12 01:29:57 +0800

    Initial Commit

---
+++ Hiep_Plists_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hiep_Plists_iOS
+//
+//  Created by Dinh Duy Hiep on 5/12/17.
+//  Copyright © 2017 Dinh Duy Hiep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
